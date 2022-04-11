887416c97054da2cdc12b344653819d38004d8da
gecho <cholakovge@gmail.com>
2022-04-11 16:45:24 +0300

    Action controller with index view

---
+++ Checlist/Checlist/Controllers/ActionController.cs
@@ -0,0 +1,12 @@
+﻿namespace Checlist.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ActionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
