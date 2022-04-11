213ab0d61201e709acc67fabd4977e9da4e37953
gecho <cholakovge@gmail.com>
2022-04-11 16:37:11 +0300

    MVC initial create with overridden identity guid identifiers

---
+++ Checlist/Checlist/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Checlist.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checlist.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Checlist/Checlist/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Checlist.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
